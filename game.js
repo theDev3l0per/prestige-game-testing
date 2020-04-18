dcde1f6f1936f785f209c2c9162cb7de1e22191a
Glitch (hello-webpage) <none>
2020-04-18 16:13:18 +0000

    🔔🎭 Checkpoint ./game.js:9783767/920 ./index.html:9783767/377 ./style.css:9783767/15

--- game.js
+++ game.js
@@ -1,13 +1,19 @@
 console.log("What are you doing here in the console?");
 var points = 0;
+var clickers = 0;
+var factories = 0;
+var portals = 0;
+var cost1 = 25;
+var cost2 = 200;
+var cost3 = 15000;
 var x = 0;
 var y = 0;
 var tickpart = 0;
-var tickspeed = 1000;
+var tickspeed = 50;
 
 function pointButton() {
   points += 1;
-  document.getElementById("number").innerHTML = "Number = " + points;
+  document.getElementById("number").innerHTML = "Number = " + Math.floor(points);
 }
 
 var mainGameLoop = window.setInterval(function() {
@@ -19,6 +25,8 @@ var mainGameLoop = window.setInterval(function() {
 }, 50);
 
 function loop() {
-  if (tickspeed<50) {
-  }
+  points += clickers/20;
+  points += factories/2;
+  points += portals*50;
+  document.getElementById("number").innerHTML = "Number = " + Math.floor(points);
 }
