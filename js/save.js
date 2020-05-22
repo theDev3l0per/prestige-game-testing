0e09fba725f72d8212984cc09eb36a09fb594f8a
Glitch (hello-webpage) <none>
2020-05-22 00:27:11 +0000

    🏬🍟 Checkpoint ./js/save.js:9783767/181:9785301/255

--- js/save.js
+++ js/save.js
@@ -6,11 +6,20 @@ function stringify() {
   }
   return temp
 }
+function decimalify(save) {
+  var i;
+  let temp = save
+  for(i in temp) {
+    temp[i] = new Decimal(temp[i].toString()) //dont worry it just doesnt know its in different file  
+  }
+  return temp
+}
+
 function save() {
   localStorage.save = btoa(JSON.stringify(stringify()))//yes you are 100% allowed to do this and it works 
 }
 function load() {
-  game = atob()
+  game = atob(JSON.parse(localStorage.save))
 }
 
 
@@ -18,5 +27,3 @@ function load() {
 
 
 
-// pls do confirmation for hard reset
-  //all that takes is if(confirm("are you sure????")){hardReset()} i knowv 
